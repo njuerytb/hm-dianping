be98998c3de43da4b540e799023d514af72ae24b
njuerytb <1374259814@qq.com>
2026-05-09 20:53:19 +0800

    添加了商品秒杀业务

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -4,6 +4,7 @@ import com.hmdp.utils.LoginInterceptor;
 import com.hmdp.utils.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
@@ -13,7 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
     @Override
-    public void addInterceptors(org.springframework.web.servlet.config.annotation.InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))
                 .excludePathPatterns(
                         "/user/code",
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,17 +3,33 @@ package com.hmdp;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 @Resource
 private ShopServiceImpl shopService;
+@Resource
+private RedisIdWorker redisIdWorker;
+private ExecutorService es= Executors.newFixedThreadPool(500);
 @Test
     void testSaveShop() throws InterruptedException {
     shopService.saveShop2Redis(1L,10L);
+}
+@Test
+    void testIdWorker() throws InterruptedException {
+    Runnable task=()->{
+        for (int i = 0; i < 1000; i++) {
+            long id = redisIdWorker.nextId("order");
+            System.out.println("id = " + id);
+        }
+    };
+    es.submit(task);
 }
 }
